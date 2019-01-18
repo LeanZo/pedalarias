79fff8579d0f0b581ce04436c103f34daff775c7
Lucas Lean Nascimento <kashchannel@gmail.com>
2019-01-17 23:56:21 -0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@ function Resposta(nivel, resposta) {
     switch (nivel) {
         case 1:
             if (resposta.toLowerCase() == "helio" || resposta.toLowerCase() == "hélio") {
-                window.location = "https://leanzo.github.io/pedalarias/Era-uma-vez.html";
+                window.location = "https://leanzo.github.io/pedalarias/valeu_sherlock_holmes.html";
+            }
+            break;
+        case 1:
+            if (resposta.toLowerCase() == "realengo" || resposta.toLowerCase() == "REALEANGO") {
+                window.location = "https://leanzo.github.io/pedalarias/tu_eh_da_tim.html";
             }
             break;
     }
