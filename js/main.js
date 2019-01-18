0f77444d16c320f2bff400752e5ea95de7e04dfe
Lucas Lean Nascimento <kashchannel@gmail.com>
2019-01-18 00:32:53 -0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,9 +5,14 @@ function Resposta(nivel, resposta) {
                 window.location = "https://leanzo.github.io/pedalarias/valeu_sherlock_holmes.html";
             }
             break;
-        case 1:
-            if (resposta.toLowerCase() == "realengo" || resposta.toLowerCase() == "REALEANGO") {
-                window.location = "https://leanzo.github.io/pedalarias/tu_eh_da_tim.html";
+        case 2:
+            if (resposta.toLowerCase() == "realengo") {
+                window.location = "https://leanzo.github.io/pedalarias/ta_sabendo.html";
+            }
+            break;
+        case 3:
+            if (resposta.toLowerCase() == "karl wilhelm reinmuth") {
+                window.location = "https://leanzo.github.io/pedalarias/ta_gostando_ne.html";
             }
             break;
     }
